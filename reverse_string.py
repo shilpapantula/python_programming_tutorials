26b7d958ef0c51b4eb35101d2686f470479b2840
mocharepo <shilpa.310@gmail.com>
2016-03-30 12:37:49 -0700

    adding testing procedure

--- reverse_string.py
+++ reverse_string.py
@@ -1,11 +1,9 @@
-string = 'anomaly'
-
 def reverse_string(string):
 	"""
 	reverses a string and returns it
 	"""
 	if len(string) < 1:
-		return []
+		return ''
 	length = len(string)
 	new_str = ''
 	for l in range(length):
@@ -13,4 +11,21 @@ def reverse_string(string):
 
 	return new_str
 
-print reverse_string(string)
+from nose.tools import assert_equal
+
+class testing(object):
+	
+	def test(self, function):
+		assert_equal(function('anomaly'), 'ylamona')
+		assert_equal(function('hello world'), 'dlrow olleh')
+		assert_equal(function('i'), 'i')
+		assert_equal(function(''), '')
+
+def main():
+	test = testing()
+	test.test(reverse_string)	
+	string = raw_input('Enter a non empty string: ')
+	print reverse_string(string)
+
+if __name__ == '__main__':
+	main()
